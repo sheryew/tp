cdcd82ff6e3b2f79a0561870b018e474f09619e5
nixonwidjaja <nixonwidjaja23@gmail.com>
2023-10-13 03:39:46 +0800

    Fix checkstyle

--- src/test/java/seedu/address/model/person/LeaveTest.java
+++ src/test/java/seedu/address/model/person/LeaveTest.java
@@ -3,8 +3,8 @@ package seedu.address.model.person;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_LEAVE;
 import static seedu.address.logic.commands.CommandTestUtil.INVALID_LEAVE;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_LEAVE;
 import static seedu.address.testutil.TypicalPersons.ALICE;
 import static seedu.address.testutil.TypicalPersons.BOB;
 
