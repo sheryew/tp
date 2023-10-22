36436402e07fc54511f395d97acfbab990a8d44b
nixonwidjaja <nixonwidjaja23@gmail.com>
2023-10-23 00:45:28 +0800

    Add tests

---
+++ src/test/java/seedu/address/model/AddressBookListTest.java
@@ -0,0 +1,35 @@
+package seedu.address.model;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
+public class AddressBookListTest {
+    private final AddressBookList addressBookList = new AddressBookList();
+
+    @Test
+    public void undo_success() {
+        addressBookList.add(new AddressBook());
+        addressBookList.add(new AddressBook());
+        assertEquals(addressBookList.undo(), new AddressBook());
+    }
+
+    @Test
+    public void undo_failure() {
+        assertThrows(IllegalArgumentException.class, () -> addressBookList.undo());
+    }
+
+    @Test
+    public void redo_success() {
+        addressBookList.add(new AddressBook());
+        addressBookList.add(new AddressBook());
+        addressBookList.undo();
+        assertEquals(addressBookList.redo(), new AddressBook());
+    }
+
+    @Test
+    public void redo_failure() {
+        assertThrows(IllegalArgumentException.class, () -> addressBookList.redo());
+    }
+}
