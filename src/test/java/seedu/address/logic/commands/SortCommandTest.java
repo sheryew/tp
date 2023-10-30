a973acbab4d55849395415b01d3705593cbc2e70
nixonwidjaja <nixonwidjaja23@gmail.com>
2023-10-31 00:28:01 +0800

    Add tests

--- src/test/java/seedu/address/logic/commands/SortCommandTest.java
+++ src/test/java/seedu/address/logic/commands/SortCommandTest.java
@@ -21,5 +21,12 @@ public class SortCommandTest {
         reversed.addPerson(ALICE);
         assertCommandSuccess(new SortCommand("name", true), model, SortCommand.MESSAGE_SUCCESS, reversed);
         assertCommandSuccess(new SortCommand("name"), model, SortCommand.MESSAGE_SUCCESS, model);
+        assertCommandSuccess(new SortCommand("phone"), model, SortCommand.MESSAGE_SUCCESS, model);
+        assertCommandSuccess(new SortCommand("email"), model, SortCommand.MESSAGE_SUCCESS, model);
+        assertCommandSuccess(new SortCommand("address"), model, SortCommand.MESSAGE_SUCCESS, model);
+        assertCommandSuccess(new SortCommand("salary"), model, SortCommand.MESSAGE_SUCCESS, model);
+        assertCommandSuccess(new SortCommand("claim"), model, SortCommand.MESSAGE_SUCCESS, model);
+        assertCommandSuccess(new SortCommand("dep"), model, SortCommand.MESSAGE_SUCCESS, model);
+        assertCommandSuccess(new SortCommand("dob"), model, SortCommand.MESSAGE_SUCCESS, model);
     }
 }
