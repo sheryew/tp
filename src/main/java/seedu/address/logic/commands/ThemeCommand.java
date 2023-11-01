90cebea244163187ddfa8dd06e822ad57a3afd35
marcellaantania <marcellaantaniatan@gmail.com>
2023-11-01 20:33:39 +0800

    Add a ThemeCommand class

---
+++ src/main/java/seedu/address/logic/commands/ThemeCommand.java
@@ -0,0 +1,22 @@
+package seedu.address.logic.commands;
+
+import seedu.address.model.Model;
+
+public class ThemeCommand extends Command {
+
+    public static final String COMMAND_WORD = "theme";
+
+    public static final String MESSAGE_THEME_CHANGE_ACKNOWLEDGEMENT = "Theme changed.";
+
+    public final String styleSheet;
+
+    public ThemeCommand(String styleSheet) {
+        this.styleSheet = styleSheet;
+    }
+
+    @Override
+    public CommandResult execute(Model model, String commandText) {
+        return new CommandResult(MESSAGE_THEME_CHANGE_ACKNOWLEDGEMENT, false,
+                false, true, styleSheet);
+    }
+}
