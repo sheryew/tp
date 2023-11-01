460797b68c288edb1ebb2414e5111d2c7ba39953
marcellaantania <marcellaantaniatan@gmail.com>
2023-11-01 20:34:07 +0800

    Add a parser for ThemeCommand (ThemeCommandParser)

---
+++ src/main/java/seedu/address/logic/parser/ThemeCommandParser.java
@@ -0,0 +1,29 @@
+package seedu.address.logic.parser;
+
+import static java.util.Objects.requireNonNull;
+
+import seedu.address.logic.Messages;
+import seedu.address.logic.commands.ThemeCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+
+public class ThemeCommandParser implements Parser<ThemeCommand> {
+
+    /**
+     * Parses {@code userInput} into a command and returns it.
+     *
+     * @param args arguments given
+     * @throws ParseException if {@code userInput} does not conform the expected format
+     */
+    @Override
+    public ThemeCommand parse(String args) throws ParseException {
+        requireNonNull(args);
+        switch (args.trim()) {
+        case "dark":
+            return new ThemeCommand("DarkTheme.css");
+        case "light":
+            return new ThemeCommand("LightTheme.css");
+        default:
+            throw new ParseException(Messages.MESSAGE_INVALID_THEME);
+        }
+    }
+}
